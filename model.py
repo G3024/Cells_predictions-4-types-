0cdd0432bc0574ff2dd3749db0caf1bcf9143cd8
Geo Yobel Effellando Gosyen <157217102+G3024@users.noreply.github.com>
2025-04-30 21:53:18 +0800

    Add files via upload

---
+++ model.py
@@ -0,0 +1,121 @@
+import tensorflow as tf
+from keras import models, layers
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+start = datetime.now()
+BATCH_SIZE = 10
+IMAGE_SIZE = 224
+CHANNELS = 1
+
+# dataset
+dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    directory = "PICS",
+    seed = 123,
+    shuffle =True,
+    image_size = (IMAGE_SIZE,IMAGE_SIZE),
+    batch_size = BATCH_SIZE
+)
+class_names = dataset.class_names
+# ['Leukemia_Cell', 'Plasmodium_Cell', 'Red_Cell', 'White_Cell']
+for image_batch, labels_batch in dataset.take(1):
+    image_batch.shape # (10, 224, 224, 3)
+    labels_batch.numpy() # [0 0 0 0 1 0 0 0 0 0]
+
+def get_dataset_partitions_tf(ds, train_split=0.8, val_split=0.1, test_split=0.1, shuffle=True, shuffle_size=100):
+    assert (train_split + test_split + val_split) == 1
+    
+    ds_size = len(ds)
+    
+    if shuffle:
+        ds = ds.shuffle(shuffle_size, seed=12)
+    
+    train_size = int(train_split * ds_size)
+    val_size = int(val_split * ds_size)
+    
+    train_ds = ds.take(train_size)    
+    val_ds = ds.skip(train_size).take(val_size)
+    test_ds = ds.skip(train_size).skip(val_size)
+    
+    return train_ds, val_ds, test_ds
+
+train_ds, val_ds, test_ds = get_dataset_partitions_tf(dataset)
+
+train_ds = train_ds.cache().shuffle(1000)
+val_ds = val_ds.cache().shuffle(1000)
+test_ds = test_ds.cache().shuffle(1000)
+
+resize_and_rescale = tf.keras.Sequential([
+  layers.Resizing(IMAGE_SIZE, IMAGE_SIZE),
+  layers.Rescaling(1./255),
+])
+
+inputshape_ = [BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, CHANNELS]
+data_augmentation = tf.keras.Sequential([
+  layers.RandomFlip("horizontal_and_vertical"),
+  layers.RandomRotation(0.2),
+])
+
+train_ds = train_ds.map(
+    lambda x, y: (data_augmentation(x, training=True), y)
+).prefetch(buffer_size=tf.data.AUTOTUNE)
+
+from keras import Input
+
+model = models.Sequential([
+    Input(shape=(224, 224, 3)),  # InputLayer'ı buraya ekleyin
+    layers.Conv2D(filters=32, kernel_size=(3,3), activation='relu'),
+    layers.MaxPooling2D(2,2),
+    
+    layers.Conv2D(filters=64, kernel_size=(3,3), activation='relu'),
+    layers.MaxPooling2D(2,2),
+    
+    layers.Flatten(),
+    layers.Dense(64, activation="relu"),
+    layers.Dense(10, activation="softmax")
+])
+model.compile(optimizer="adam",
+              loss="sparse_categorical_crossentropy",
+              metrics=["accuracy"])
+
+history=model.fit(train_ds, batch_size=10,epochs=2, validation_data=(val_ds))
+
+
+
+import numpy as np
+for images_batch, labels_batch in test_ds.take(1):
+    
+    first_image = images_batch[0].numpy().astype('uint8')
+    first_label = labels_batch[0].numpy()
+    
+    print("first image to predict")
+    plt.imshow(first_image)
+    print("actual label:",class_names[first_label])
+    
+    batch_prediction = model.predict(images_batch)
+    print("predicted label:",class_names[np.argmax(batch_prediction[0])])
+
+
+def predict(model, img):
+    img_array = tf.keras.preprocessing.image.img_to_array(images[i].numpy())
+    img_array = tf.expand_dims(img_array, 0)
+
+    predictions = model.predict(img_array)
+
+    predicted_class = class_names[np.argmax(predictions[0])]
+    confidence = round(100 * (np.max(predictions[0])), 2)
+    return predicted_class, confidence
+
+plt.figure(figsize=(15, 15))
+for images, labels in test_ds.take(1):
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        
+        predicted_class, confidence = predict(model, images[i].numpy())
+        actual_class = class_names[labels[i]] 
+        
+        plt.title(f"Actual: {actual_class},\n Predicted: {predicted_class}.\n Confidence: {confidence}%")
+        
+        plt.axis("off")
+print("model accuracy: ", model.evaluate(test_ds))
